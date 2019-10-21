61924555788bbdba80c15f96d1a2530cd350312a
helenecourau <helene.courau@gmail.com>
2019-10-21 10:06:48 +0200

    changes on test parser

--- backend/scripts/test_parser.py
+++ backend/scripts/test_parser.py
@@ -7,12 +7,14 @@ class TestParser:
 		file = "backend/scripts/resources/stop_words_fr.json"
 		user_value = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
 		wanted_value = "OpenClassrooms"
-		test_value = script.parser(user_value, file)
+		parser = script.ParserUser()
+		test_value = parser.parser(user_value, file)
 		assert test_value == wanted_value.lower()
 
 	def test_return_string_error_value(user_value):
 		file = "backend/scripts/resources/stop_words_fr.json"
 		user_value = "Salut GrandPy ! "
 		wanted_value = ""
-		test_value = script.parser(user_value, file)
+		parser = script.ParserUser()
+		test_value = parser.parser(user_value, file)
 		assert test_value == wanted_value
